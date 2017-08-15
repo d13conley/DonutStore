20b856649d6b8415e39d72389a8c70cdb8a7ca1d
Janette <jaaylopez3@gmail.com>
2017-08-15 09:58:59 -0700

    Angular

---
+++ DonutStore.Web/wwwroot/NgApp/app.js
@@ -0,0 +1,14 @@
+﻿var myApp = angular.modules("myApp", ["ui-router"]);
+myApp.controller("MainController", MainController);
+
+myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider.state('main', {
+        url: '/',
+        templateUrl: '/NgApp/Views/main.html',
+        controller: MainController,
+        controllerAs: 'controller'
+
+    });
+    $urlRouterProvider.otherwise('/notFound');
+    $locationProvider.html5Mode(true);
+});
